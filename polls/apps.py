75a986a0e44758de280e17013ea528ab07d9f537
Jemma Stones <jemmastones@SVN-15-074-Jemma.local>
2018-01-13 17:52:18 +1100

    Fixed static rename issue

--- polls/apps.py
+++ polls/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    name = 'static'
+    name = 'polls'
--- polls/views.py
+++ polls/views.py
@@ -7,21 +7,22 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-    template_name = 'static/index.html'
+    template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'static/detail.html'
+    template_name = 'polls/detail.html'
 
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'static/results.html'
+    template_name = 'polls/results.html'
 
 # Create your views here.
 
@@ -31,7 +32,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'static/detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
@@ -41,4 +42,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('static:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
